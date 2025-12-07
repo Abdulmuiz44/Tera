16bf5f2e9383d6a704aa7b8bedb1fd6d7e1b38f8
Abdulmuiz44 <abdulmuizproject@gmail.com>
2025-12-07 02:13:52 +0100

    Fix AppLayout import syntax
    - Consolidated React imports in AppLayout.tsx

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, type ReactNode } from 'react'
+import { useState, ReactNode } from 'react'
 import Sidebar from './Sidebar'
 import { useAuth } from './AuthProvider'
 
